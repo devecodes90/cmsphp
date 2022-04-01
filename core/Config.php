790773a34ba793fa44b3453a8be10170d68eae3c
benaichacodes <deve90codes@yahoo.com>
2022-04-01 12:59:19 +0000

    started getting the URL

---
+++ core/Config.php
@@ -0,0 +1,21 @@
+<?php
+namespace Core;
+class Config
+{
+  private static $config = [
+          'version'            => '0.0.1',
+          'root_dir'           => '/cmsphp', // this will likely be / on a live server
+          'default_controller' => 'Blog',    // the default home controller
+          'default_layout'     => 'default', // Default layout that will be used
+          'default_site_title' => 'Benaichacodes', // Default site title
+          'db_home_dir'        => '127.0.0.1', // DB host, please use IP address instead of domain
+          'db_name'            => 'php_cms',  // DB name
+          'db_user'            => 'root',    // DB user
+          'db_pass'            => '',  // DB password
+          'login_cookies_name' => 'hakdjri2341l8a' ,    //login_cookies_name
+      ];
+
+      public static function get($key){
+          return array_key_exists($key, self::$config)? self::$config[$key]: NULL;
+      }
+}
